3227b3d0cd51993fde7d7bffa03d7018a9b45a41
Ben Reinhart <benjreinhart@gmail.com>
2011-10-04 15:10:48 -0500

    updated spec file for better readability

--- spec/javascripts/DecodeSpec.js
+++ spec/javascripts/DecodeSpec.js
@@ -1,11 +1,15 @@
-describe("Decoding", function() {
-  it("should return 'string'", function() {
-    bencodedString = '6:string';
+describe("Decoding A String", function() {
+  var bencodedString = '6:string';
+
+  it("should return a string of value 'helloworld' when given the bencoded string '10:helloworld'", function() {
     expect( bencodedString.decode() ).toEqual( 'string' );
   });
+});
 
-  it("should return an Number with value 123", function() {
-    bencodedString = 'i123e';
+describe("Decoding A Number", function() {
+  var bencodedString = 'i123e';
+
+  it("should return an integer of value 123 when given the bencoded string 'i123e'", function() {
     expect( bencodedString.decode() ).toEqual( 123 );
   });
 });
