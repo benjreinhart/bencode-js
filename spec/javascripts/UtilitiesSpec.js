51bebef259445b3b2140256b6ca1a4902d33c37f
Ben Reinhart <benjreinhart@gmail.com>
2011-10-07 00:05:40 -0500

    specified load order, added utility functions

---
+++ spec/javascripts/UtilitiesSpec.js
@@ -0,0 +1,34 @@
+describe("Utility Functions", function() {
+
+  describe("Array.__each", function() {
+      var array = ["obj1", "obj2", "obj3"]
+        , newArray
+      ;
+
+      var iterator = function( item ) {
+        item = '' + item + ' i';
+        return newArray.push(item);
+      }
+
+      beforeEach(function() { newArray = []; });
+
+    it("should iterate appropriately", function() {
+      array.__each( iterator, this);
+      expect( newArray ).toEqual( ["obj1 i", "obj2 i", "obj3 i"] );
+    });
+
+    it("should break iteration when iterator returns new __Breaker", function() {
+      var iterator = function( item ) {
+        if ( item === "obj2" ) return new __Breaker;
+        item = '' + item + ' i';
+        return newArray.push(item);
+      }
+      array.__each( iterator, this);
+      expect( newArray ).toEqual( ["obj1 i"] );
+    });
+  });
+
+  describe("Array.__each", function() {
+
+  });
+});
