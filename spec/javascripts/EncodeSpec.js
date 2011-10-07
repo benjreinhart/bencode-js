e62f9615c19c538384b646b5749649382bd194b5
Ben Reinhart <benjreinhart@gmail.com>
2011-10-07 15:41:43 -0500

    refactored/added more specs for encode

--- spec/javascripts/EncodeSpec.js
+++ spec/javascripts/EncodeSpec.js
@@ -12,22 +12,29 @@ describe("Encoding An Integer", function() {
   it("should return a bencoded string of value 'i12345e'", function() {
     expect( regularInteger.encode() ).toEqual( "i12345e" );
   });
+
+  it("should return properly bencoded string of value 'i-1234e' when number is negative", function() {
+    var num = -1234;
+    expect( num.encode() ).toEqual( "i-1234e" );
+  });
 });
 
 describe("Encoding An Array", function() {
-  var array = ["Benny", 123, "john", ["tom", 923] ];
+  var array = ["Benny", 123, "john", ["tom", -923], { one: "value one" } ];
+
+  var bencodedString = "l5:Bennyi123e4:johnl3:tomi-923eed3:one9:value oneee";
 
   it("should return a properly bencoded array", function() {
-    expect( array.encode() ).toEqual( "l5:Bennyi123e4:johnl3:tomi923eee" );
+    expect( array.encode() ).toEqual( bencodedString );
   });
 });
 
 describe("Encoding An Object", function() {
   var object = { one: "ben", two: ["john", 123, "james"], three: { key: "value" } };
 
-  var expectedString = "d3:one3:ben3:twol4:johni123e5:jamese5:threed3:key5:valueee";
+  var bencodedString = "d3:one3:ben3:twol4:johni123e5:jamese5:threed3:key5:valueee";
 
   it("should return a properly bencoded Object", function() {
-    expect( object.encode() ).toEqual( expectedString );
+    expect( object.encode() ).toEqual( bencodedString );
   });
 });
