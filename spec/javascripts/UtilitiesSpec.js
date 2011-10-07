fb996913da270e67ddcd83cb8d36c7dcb8fb6c51
Ben Reinhart <benjreinhart@gmail.com>
2011-10-07 00:45:10 -0500

    added specs for Object.__each

--- spec/javascripts/UtilitiesSpec.js
+++ spec/javascripts/UtilitiesSpec.js
@@ -17,7 +17,7 @@ describe("Utility Functions", function() {
       expect( newArray ).toEqual( ["obj1 i", "obj2 i", "obj3 i"] );
     });
 
-    it("should break iteration when iterator returns new __Breaker", function() {
+    it("should break iteration when iterator returns an instance of __Breaker", function() {
       var iterator = function( item ) {
         if ( item === "obj2" ) return new __Breaker;
         item = '' + item + ' i';
@@ -30,5 +30,30 @@ describe("Utility Functions", function() {
 
   describe("Object.__each", function() {
 
+    var obj    = { obj1: "someVal 1", obj2: "someVal 2", obj3: "someVal 3" }
+      , newObj
+    ;
+
+    var iterator = function( item, key, obj ) {
+      value = '' + item + ' i';
+      return newObj[key] = value;
+    }
+
+    beforeEach(function() { newObj = {} });
+
+    it("should iteratore appropriately", function() {
+      obj.__each( iterator, this );
+      expect( newObj ).toEqual( { obj1: "someVal 1 i", obj2: "someVal 2 i", obj3: "someVal 3 i" } );
+    });
+
+    it("should break iteratio when iterator returns an instance of __Breaker", function() {
+      var iterator = function( item, key, obj ) {
+        if ( key === "obj2" ) return new __Breaker;
+        value = '' + item + ' i';
+        return newObj[key] = value;
+      }
+      obj.__each( iterator, this );
+      expect( newObj ).toEqual( { obj1: "someVal 1 i"} );
+    });
   });
 });
